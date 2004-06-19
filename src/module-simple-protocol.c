b4e3f5c5c5fb1cb0ddb10d123d69c211516c4ac7
Lennart Poettering <lennart@poettering.net>
2004-06-19 18:41:24 +0000

    add simple ptorocol with unix
    git-svn-id: file:///home/lennart/svn/public/pulseaudio/trunk@25 fefdeb5f-60dc-0310-8127-8f9354f1896f

--- src/module-simple-protocol.c
+++ src/module-simple-protocol.c
@@ -9,8 +9,13 @@ int module_init(struct core *c, struct module*m) {
     struct socket_server *s;
     assert(c && m);
 
+#ifdef USE_TCP_SOCKETS
     if (!(s = socket_server_new_ipv4(c->mainloop, INADDR_LOOPBACK, 4712)))
         return -1;
+#else
+    if (!(s = socket_server_new_unix(c->mainloop, "/tmp/polypsimple")))
+        return -1;
+#endif
 
     m->userdata = protocol_simple_new(c, s, PROTOCOL_SIMPLE_PLAYBACK);
     assert(m->userdata);
