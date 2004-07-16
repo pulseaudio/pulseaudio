e0d510df922d4c3440944a3c9670e52ad668056d
Lennart Poettering <lennart@poettering.net>
2004-07-16 19:56:36 +0000

    include copyright and svn tag in *.[ch]
    git-svn-id: file:///home/lennart/svn/public/pulseaudio/trunk@83 fefdeb5f-60dc-0310-8127-8f9354f1896f

--- src/sconv-s16le.h
+++ src/sconv-s16le.h
@@ -1,6 +1,27 @@
 #ifndef foosconv_s16lefoo
 #define foosconv_s16lefoo
 
+/* $Id$ */
+
+/***
+  This file is part of polypaudio.
+ 
+  polypaudio is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published
+  by the Free Software Foundation; either version 2 of the License,
+  or (at your option) any later version.
+ 
+  polypaudio is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with polypaudio; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+  USA.
+***/
+
 void pa_sconv_s16le_to_float32(unsigned n, const void *a, unsigned an, float *b);
 void pa_sconv_s16le_from_float32(unsigned n, const float *a, void *b, unsigned bn);
 
