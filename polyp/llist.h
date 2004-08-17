ac595189fdc311ae30fa8061cd5bbaeb72fc0ef1
Lennart Poettering <lennart@poettering.net>
2004-08-17 19:47:42 +0000

    add missing copyright headers
    git-svn-id: file:///home/lennart/svn/public/pulseaudio/trunk@138 fefdeb5f-60dc-0310-8127-8f9354f1896f

--- polyp/llist.h
+++ polyp/llist.h
@@ -1,6 +1,27 @@
 #ifndef foollistfoo
 #define foollistfoo
 
+/* $Id$ */
+
+/***
+  This file is part of polypaudio.
+ 
+  polypaudio is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published
+  by the Free Software Foundation; either version 2 of the License,
+  or (at your option) any later version.
+ 
+  polypaudio is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with polypaudio; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+  USA.
+***/
+
 #define PA_LLIST_HEAD(t,name) t *name
 
 #define PA_LLIST_FIELDS(t) t *next, *prev;
