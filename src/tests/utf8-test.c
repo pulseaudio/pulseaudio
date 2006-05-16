53595938d003411e6992e10b6a5c5e51b2a15d4f
Lennart Poettering <lennart@poettering.net>
2006-05-16 00:46:03 +0000

    add new test programme utf8-test.c
    git-svn-id: file:///home/lennart/svn/public/pulseaudio/trunk@880 fefdeb5f-60dc-0310-8127-8f9354f1896f

---
+++ src/tests/utf8-test.c
@@ -0,0 +1,26 @@
+/* $Id$ */
+
+#include <stdio.h>
+#include <assert.h>
+
+#include <polypcore/utf8.h>
+#include <polypcore/xmalloc.h>
+
+int main(int argc, char *argv[]) {
+    char *c;
+    
+    assert(pa_utf8_valid("hallo"));
+    assert(pa_utf8_valid("hallo\n"));
+    assert(!pa_utf8_valid("hüpfburg\n"));
+    assert(pa_utf8_valid("hallo\n"));
+    assert(pa_utf8_valid("hÃ¼pfburg\n"));
+    
+    printf("LATIN1: %s\n", c = pa_utf8_filter("hüpfburg"));
+    pa_xfree(c);
+    printf("UTF8: %sx\n", c = pa_utf8_filter("hÃ¼pfburg"));
+    pa_xfree(c);
+    printf("LATIN1: %sx\n", c = pa_utf8_filter("üxknärzmörzeltörszß³§dsjkfh"));
+    pa_xfree(c);
+
+    return 0;
+}
