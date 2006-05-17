6ab421381ab9047d92f2679b67a8a4e0ea611d11
Pierre Ossman <ossman@cendio.se>
2006-05-17 11:10:59 +0000

    Update documentation to contain the UTF-8 requirement.
    git-svn-id: file:///home/lennart/svn/public/pulseaudio/trunk@893 fefdeb5f-60dc-0310-8127-8f9354f1896f

--- src/polyp/polypaudio.h
+++ src/polyp/polypaudio.h
@@ -54,6 +54,11 @@
  * \li The complete but somewhat complicated to use asynchronous API
  * \li The simplified, easy to use, but limited synchronous API
  *
+ * All strings in Polypaudio are in the UTF-8 encoding, regardless of current
+ * locale. Some functions will filter invalid sequences from the string, some
+ * will simply fail. To ensure reliable behaviour, make sure everything you
+ * pass to the API is already in UTF-8.
+ 
  * \section simple_sec Simple API
  *
  * Use this if you develop your program in synchronous style and just
