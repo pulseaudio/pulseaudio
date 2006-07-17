64d87ac36329d76d220c437bf56c665f1f839ea5
Lennart Poettering <lennart@poettering.net>
2006-07-17 21:20:31 +0000

    change licensing blurb form "Library GPL" to "Lesser GPL" on request of Loic Minier. Effectively this means using the same license blurb like in all other source files.
    git-svn-id: file:///home/lennart/svn/public/pulseaudio/trunk@1099 fefdeb5f-60dc-0310-8127-8f9354f1896f

--- src/pulsecore/poll.h
+++ src/pulsecore/poll.h
@@ -6,21 +6,25 @@
    Copyright (C) 2005, Cendio AB.
    This file is part of PulseAudio.
    Based on work for the GNU C Library.
+***/
 
-   PulseAudio is free software; you can redistribute it and/or modify it
-   under the terms of the GNU Library General Public License as published
-   by the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-
-   PulseAudio is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Library General Public
-   License along with PulseAudio; If not, write to the Free Software
-   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
-   USA.
+/***
+  This file is part of PulseAudio.
+ 
+  PulseAudio is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published
+  by the Free Software Foundation; either version 2 of the License,
+  or (at your option) any later version.
+ 
+  PulseAudio is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License for more details.
+ 
+  You should have received a copy of the GNU Lesser General Public License
+  along with PulseAudio; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+  USA.
 ***/
 
 /* Event types that can be polled for.  These bits may be set in `events'
