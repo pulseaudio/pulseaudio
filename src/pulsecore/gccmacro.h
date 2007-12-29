9d00b9dea2b236c3bc6cbf8f9b5ff87fe6aa67b3
Lennart Poettering <lennart@poettering.net>
2007-12-29 18:04:31 +0000

    convert argument to boolean value before passing it on to __builtin_expect in PA_LIKELY
    git-svn-id: file:///home/lennart/svn/public/pulseaudio/trunk@2090 fefdeb5f-60dc-0310-8127-8f9354f1896f

--- src/pulsecore/gccmacro.h
+++ src/pulsecore/gccmacro.h
@@ -79,7 +79,7 @@
 
 #ifndef PA_LIKELY
 #ifdef __GNUC__
-#define PA_LIKELY(x) (__builtin_expect((x),1))
+#define PA_LIKELY(x) (__builtin_expect(!!(x),1))
 #define PA_UNLIKELY(x) (__builtin_expect((x),0))
 #else
 #define PA_LIKELY(x) (x)
