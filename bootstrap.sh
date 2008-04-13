91f092eadcc5e9075e04ae42df11de28ef5047a8
Sjoerd Simons <sjoerd@debian.org>
2008-04-13 19:05:53 +0000

    Let bootstrap.sh require version 1.10 of the various autofoo bits. Otherwise things will break during compilation
    git-svn-id: file:///home/lennart/svn/public/pulseaudio/trunk@2263 fefdeb5f-60dc-0310-8127-8f9354f1896f

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,7 +17,7 @@
 # along with PulseAudio; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
 
-VERSION=1.9
+VERSION=1.10
 
 run_versioned() {
     local P
