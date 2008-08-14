fcd7dc1fd715b13f7fe29f8041a4e00a3666af2e
João Paulo Rechi Vita <joao.vita@gmail.com>
2008-08-14 18:12:26 -0300

    Add include for core-util.h

--- src/modules/module-bt-device.c
+++ src/modules/module-bt-device.c
@@ -30,6 +30,7 @@
 #include <pulse/xmalloc.h>
 #include <pulse/timeval.h>
 #include <pulse/sample.h>
+#include <pulsecore/core-util.h>
 #include <pulsecore/core-error.h>
 #include <pulsecore/module.h>
 #include <pulsecore/modargs.h>
