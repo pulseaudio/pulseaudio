c2450501af82d1c9d1994e4f4ce80d506d3c90ae
Lennart Poettering <lennart@poettering.net>
2009-01-08 01:03:42 +0100

    Prefer mixer controls with volumes over switches
    When we look for a mixer control prefer controls that have both volume
    and a mute switch over those that have only a volume switch over those
    that only have a mute switch.
    
    Originally pointed out by Adel Gadllah.

--- src/modules/alsa-util.h
+++ src/modules/alsa-util.h
@@ -52,7 +52,7 @@ int pa_alsa_set_hw_params(
 int pa_alsa_set_sw_params(snd_pcm_t *pcm, snd_pcm_uframes_t avail_min);
 
 int pa_alsa_prepare_mixer(snd_mixer_t *mixer, const char *dev);
-snd_mixer_elem_t *pa_alsa_find_elem(snd_mixer_t *mixer, const char *name, const char *fallback);
+snd_mixer_elem_t *pa_alsa_find_elem(snd_mixer_t *mixer, const char *name, const char *fallback, pa_bool_t playback);
 
 snd_pcm_t *pa_alsa_open_by_device_id(
         const char *dev_id,
