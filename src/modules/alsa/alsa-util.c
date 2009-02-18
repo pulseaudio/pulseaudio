07333f8cae480a25af02ad32286ab38d4272698e
Lennart Poettering <lennart@poettering.net>
2009-02-18 21:50:27 +0100

    refer folks to the ALSA devs, not us

--- src/modules/alsa/alsa-util.c
+++ src/modules/alsa/alsa-util.c
@@ -1553,8 +1553,8 @@ snd_pcm_sframes_t pa_alsa_safe_avail_update(snd_pcm_t *pcm, size_t hwbuf_size, c
 
     if (k >= hwbuf_size * 3 ||
         k >= pa_bytes_per_second(ss)*10)
-        pa_log("snd_pcm_avail_update() returned a value that is exceptionally large: %lu bytes (%lu ms) "
-               "Most likely this is an ALSA driver bug. Please report this issue to the PulseAudio developers.",
+        pa_log("snd_pcm_avail_update() returned a value that is exceptionally large: %lu bytes (%lu ms). "
+               "Most likely this is an ALSA driver bug. Please report this issue to the ALSA developers.",
                (unsigned long) k, (unsigned long) (pa_bytes_to_usec(k, ss) / PA_USEC_PER_MSEC));
 
     return n;
@@ -1585,8 +1585,8 @@ int pa_alsa_safe_mmap_begin(snd_pcm_t *pcm, const snd_pcm_channel_area_t **areas
         k >= hwbuf_size * 3 ||
         k >= pa_bytes_per_second(ss)*10)
 
-        pa_log("snd_pcm_mmap_begin() returned a value that is exceptionally large: %lu bytes (%lu ms) "
-               "Most likely this is an ALSA driver bug. Please report this issue to the PulseAudio developers.",
+        pa_log("snd_pcm_mmap_begin() returned a value that is exceptionally large: %lu bytes (%lu ms). "
+               "Most likely this is an ALSA driver bug. Please report this issue to the ALSA developers.",
                (unsigned long) k, (unsigned long) (pa_bytes_to_usec(k, ss) / PA_USEC_PER_MSEC));
 
     return r;
