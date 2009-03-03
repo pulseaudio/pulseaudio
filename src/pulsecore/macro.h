86dee05aec330a0c2886c0327712153793ca46c4
Colin Guthrie <cguthrie@mandriva.org>
2009-03-03 20:23:02 +0000

    Use LGPL 2.1 on all files previously using LGPL 2

--- src/pulsecore/macro.h
+++ src/pulsecore/macro.h
@@ -8,7 +8,7 @@
 
   PulseAudio is free software; you can redistribute it and/or modify
   it under the terms of the GNU Lesser General Public License as published
-  by the Free Software Foundation; either version 2 of the License,
+  by the Free Software Foundation; either version 2.1 of the License,
   or (at your option) any later version.
 
   PulseAudio is distributed in the hope that it will be useful, but
