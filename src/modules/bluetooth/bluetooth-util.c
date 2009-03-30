ff7033c11d9248fe837204b03c8397231dc511fe
Lennart Poettering <lennart@poettering.net>
2009-03-31 00:22:17 +0200

    Revert "make sure we always read in all properties"
    This reverts commit 1c8f968282208be391f062c1656d85e2ba5078ac.
    
    Turns out doing this is racy. The appropriate fix seems to be
    38825d79123678bf0c5d156aaea4bedb888a7fcd

--- src/modules/bluetooth/bluetooth-util.c
+++ src/modules/bluetooth/bluetooth-util.c
@@ -418,15 +418,6 @@ static void found_device(pa_bluetooth_discovery *y, const char* path) {
 
     pa_assert_se(m = dbus_message_new_method_call("org.bluez", path, "org.bluez.Device", "GetProperties"));
     send_and_add_to_pending(y, d, m, get_properties_reply);
-
-    pa_assert_se(m = dbus_message_new_method_call("org.bluez", path, "org.bluez.Audio", "GetProperties"));
-    send_and_add_to_pending(y, d, m, get_properties_reply);
-
-    pa_assert_se(m = dbus_message_new_method_call("org.bluez", path, "org.bluez.Headset", "GetProperties"));
-    send_and_add_to_pending(y, d, m, get_properties_reply);
-
-    pa_assert_se(m = dbus_message_new_method_call("org.bluez", path, "org.bluez.AudioSink", "GetProperties"));
-    send_and_add_to_pending(y, d, m, get_properties_reply);
 }
 
 static void list_devices_reply(DBusPendingCall *pending, void *userdata) {
