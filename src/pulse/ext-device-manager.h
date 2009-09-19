9357bdf4e7c069e29d2fa403a01a892d80d2d89f
Colin Guthrie <cguthrie@mandriva.org>
2009-09-19 15:32:13 +0100

    device-manager: Update docs version -> 0.9.19 (predicted)

--- src/pulse/ext-device-manager.h
+++ src/pulse/ext-device-manager.h
@@ -34,38 +34,38 @@
 PA_C_DECL_BEGIN
 
 /** Stores information about one device in the device database that is
- * maintained by module-device-manager. \since 0.9.17 */
+ * maintained by module-device-manager. \since 0.9.19 */
 typedef struct pa_ext_device_manager_info {
     const char *name;            /**< Identifier string of the device. A string like "sink:" or similar followed by the name of the device. */
     const char *description;     /**< The description of the device when it was last seen, if applicable and saved */
 } pa_ext_device_manager_info;
 
-/** Callback prototype for pa_ext_device_manager_test(). \since 0.9.17 */
+/** Callback prototype for pa_ext_device_manager_test(). \since 0.9.19 */
 typedef void (*pa_ext_device_manager_test_cb_t)(
         pa_context *c,
         uint32_t version,
         void *userdata);
 
-/** Test if this extension module is available in the server. \since 0.9.17 */
+/** Test if this extension module is available in the server. \since 0.9.19 */
 pa_operation *pa_ext_device_manager_test(
         pa_context *c,
         pa_ext_device_manager_test_cb_t cb,
         void *userdata);
 
-/** Callback prototype for pa_ext_device_manager_read(). \since 0.9.17 */
+/** Callback prototype for pa_ext_device_manager_read(). \since 0.9.19 */
 typedef void (*pa_ext_device_manager_read_cb_t)(
         pa_context *c,
         const pa_ext_device_manager_info *info,
         int eol,
         void *userdata);
 
-/** Read all entries from the device database. \since 0.9.17 */
+/** Read all entries from the device database. \since 0.9.19 */
 pa_operation *pa_ext_device_manager_read(
         pa_context *c,
         pa_ext_device_manager_read_cb_t cb,
         void *userdata);
 
-/** Store entries in the device database. \since 0.9.17 */
+/** Store entries in the device database. \since 0.9.19 */
 pa_operation *pa_ext_device_manager_write(
         pa_context *c,
         pa_update_mode_t mode,
@@ -75,27 +75,27 @@ pa_operation *pa_ext_device_manager_write(
         pa_context_success_cb_t cb,
         void *userdata);
 
-/** Delete entries from the device database. \since 0.9.17 */
+/** Delete entries from the device database. \since 0.9.19 */
 pa_operation *pa_ext_device_manager_delete(
         pa_context *c,
         const char *const s[],
         pa_context_success_cb_t cb,
         void *userdata);
 
-/** Subscribe to changes in the device database. \since 0.9.17 */
+/** Subscribe to changes in the device database. \since 0.9.19 */
 pa_operation *pa_ext_device_manager_subscribe(
         pa_context *c,
         int enable,
         pa_context_success_cb_t cb,
         void *userdata);
 
-/** Callback prototype for pa_ext_device_manager_set_subscribe_cb(). \since 0.9.17 */
+/** Callback prototype for pa_ext_device_manager_set_subscribe_cb(). \since 0.9.19 */
 typedef void (*pa_ext_device_manager_subscribe_cb_t)(
         pa_context *c,
         void *userdata);
 
 /** Set the subscription callback that is called when
- * pa_ext_device_manager_subscribe() was called. \since 0.9.17 */
+ * pa_ext_device_manager_subscribe() was called. \since 0.9.19 */
 void pa_ext_device_manager_set_subscribe_cb(
         pa_context *c,
         pa_ext_device_manager_subscribe_cb_t cb,
