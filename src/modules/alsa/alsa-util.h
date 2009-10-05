833bf30cb9a4ce10774abfe0b916da99709fbb0a
Lennart Poettering <lennart@poettering.net>
2009-10-06 00:17:24 +0200

    alsa: disable period event only with tsched=1

--- src/modules/alsa/alsa-util.h
+++ src/modules/alsa/alsa-util.h
@@ -51,7 +51,8 @@ int pa_alsa_set_hw_params(
 
 int pa_alsa_set_sw_params(
         snd_pcm_t *pcm,
-        snd_pcm_uframes_t avail_min);
+        snd_pcm_uframes_t avail_min,
+        pa_bool_t period_event);
 
 /* Picks a working mapping from the profile set based on the specified ss/map */
 snd_pcm_t *pa_alsa_open_by_device_id_auto(
