8ec304d2d1e956cc3f5f35437ac4fe580b36f004
Diego Elio 'Flameeyes' Pettenò <flameeyes@gmail.com>
2009-10-07 19:31:11 +0200

    Fix build when using -fweb, accept both register and memory constraints.
    This was reported as Gentoo bug #287391 by Torsten Kaiser, and the fix was
    suggested by Mike Frysinger.

--- src/pulsecore/svolume_sse.c
+++ src/pulsecore/svolume_sse.c
@@ -149,7 +149,7 @@ pa_volume_s16ne_sse2 (int16_t *samples, int32_t *volumes, unsigned channels, uns
         "8:                             \n\t"
 
         : "+r" (samples), "+r" (volumes), "+r" (length), "=D" (channel), "=&r" (temp)
-        : "X" ((pa_reg_x86)channels)
+        : "rm" ((pa_reg_x86)channels)
         : "cc"
     );
 }
@@ -237,7 +237,7 @@ pa_volume_s16re_sse2 (int16_t *samples, int32_t *volumes, unsigned channels, uns
         "8:                             \n\t"
 
         : "+r" (samples), "+r" (volumes), "+r" (length), "=D" (channel), "=&r" (temp)
-        : "X" ((pa_reg_x86)channels)
+        : "rm" ((pa_reg_x86)channels)
         : "cc"
     );
 }
