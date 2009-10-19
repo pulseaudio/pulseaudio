542fa468c26c5035485dc73392c57c222183b510
Daniel Mack <daniel@caiaq.de>
2009-10-19 12:48:00 +0200

    Mac OS X: add semaphore implementation
    On Wed, Sep 16, 2009 at 11:57:04PM +0200, Lennart Poettering wrote:
    > On Wed, 16.09.09 15:15, Daniel Mack (daniel@caiaq.de) wrote:
    >
    > > +    s = pa_xnew(pa_semaphore, 1);
    > > +    MPCreateSemaphore(UINT_MAX, value, &(s->sema));
    > > +    pa_assert(s->sema != 0);
    >
    > Hmm, I'd prefer if the ret val of MPCreateSemaphore() would be checked
    > here.
    >
    > Also I find it a bit weird checking for s->sema, though not
    > initializing it to 0 in the beginning. If the call actually failed,
    > then the assert will check uninitialized memory. Also, comparing
    > pointers with 0 sucks. That should be NULL.
    >
    > Given that this can not realisitically fail, only in OOM or OOM-like
    > situations in which case we abort anyway it mght be enough just writing:
    >
    > pa_assert_se(MPCreateSemaphore(UINT_MAX, value, &s->sema) == 0);
    >
    > (Assuming that success is signalled by retval == 0 on MacOSX)
    >
    > > +void pa_semaphore_free(pa_semaphore *s) {
    > > +    pa_assert(s);
    > > +    MPDeleteSemaphore(s->sema);
    >
    > Same here.
    >
    > > +    pa_xfree(s);
    > > +}
    > > +
    > > +void pa_semaphore_post(pa_semaphore *s) {
    > > +    pa_assert(s);
    > > +    MPSignalSemaphore(s->sema);
    >
    > And here.
    >
    > > +}
    > > +
    > > +void pa_semaphore_wait(pa_semaphore *s) {
    > > +    pa_assert(s);
    > > +    /* should probably check return value (-ve is error), noErr is ok. */
    > > +    MPWaitOnSemaphore(s->sema, kDurationForever);
    >
    > And here.
    
    Ok, done. See the patch below.
    
    Daniel
    
    >From 26df2fbae6d9215a3ae084876fb5f79e4d9cf4f0 Mon Sep 17 00:00:00 2001
    From: Kim Lester <kim@dfusion.com.au>
    Date: Wed, 16 Sep 2009 09:23:39 +0800
    Subject: [PATCH] Mac OS X: add semaphore implementation

---
+++ src/pulsecore/semaphore-osx.c
@@ -0,0 +1,63 @@
+/***
+  This file is part of PulseAudio.
+
+  Copyright 2009 Kim Lester <kim@dfusion.com.au>
+
+  PulseAudio is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published
+  by the Free Software Foundation; either version 2.1 of the License,
+  or (at your option) any later version.
+
+  PulseAudio is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with PulseAudio; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+  USA.
+***/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <Multiprocessing.h>
+
+#include <pulse/xmalloc.h>
+#include <pulsecore/macro.h>
+
+#include "semaphore.h"
+
+struct pa_semaphore
+{
+    MPSemaphoreID sema;
+};
+
+pa_semaphore* pa_semaphore_new(unsigned int value) {
+    /* NOTE: Can't assume boolean - ie value = 0,1, so use UINT_MAX (boolean more efficient ?) */
+    pa_semaphore *s;
+
+    s = pa_xnew(pa_semaphore, 1);
+    pa_assert_se(MPCreateSemaphore(UINT_MAX, value, &s->sema) == 0);
+
+    return s;
+}
+
+void pa_semaphore_free(pa_semaphore *s) {
+    pa_assert(s);
+    pa_assert_se(MPDeleteSemaphore(s->sema) == 0);
+    pa_xfree(s);
+}
+
+void pa_semaphore_post(pa_semaphore *s) {
+    pa_assert(s);
+    pa_assert_se(MPSignalSemaphore(s->sema) == 0);
+}
+
+void pa_semaphore_wait(pa_semaphore *s) {
+    pa_assert(s);
+    /* should probably check return value (-ve is error), noErr is ok. */
+    pa_assert_se(MPWaitOnSemaphore(s->sema, kDurationForever) == 0);
+}
