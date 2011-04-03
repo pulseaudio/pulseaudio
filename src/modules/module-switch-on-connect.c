6ed3a7dcc9de81469c5d35a5256e5f50bd48f804
Michael Terry <michael.terry@canonical.com>
2011-04-03 13:42:44 +0100

    switch-on-connect: Add a new module to allow for hotplugged devices to be used by default.
    This module implements a simply policy decision that any newly plugged
    in devices should be used.
    
    This is a reasonable approach and paprefs will be updated to allow for
    this option to be turned on or off.
    
    This is more or less a stop-gap solution. When priority lists are
    implemented in the core, then policy modules may ultimately be
    re-engineered to adjust the priority lists rather than doing any of
    their own routing per-se.

---
+++ src/modules/module-switch-on-connect.c
@@ -0,0 +1,187 @@
+/***
+  This file is part of PulseAudio.
+
+  Copyright 2006 Lennart Poettering
+  Copyright 2009 Canonical Ltd
+
+  PulseAudio is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published
+  by the Free Software Foundation; either version 2.1 of the License,
+  or (at your option) any later version.
+
+  PulseAudio is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with PulseAudio; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+  USA.
+***/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <pulse/xmalloc.h>
+
+#include <pulsecore/core.h>
+#include <pulsecore/sink-input.h>
+#include <pulsecore/source-output.h>
+#include <pulsecore/modargs.h>
+#include <pulsecore/log.h>
+#include <pulsecore/namereg.h>
+#include <pulsecore/core-util.h>
+
+#include "module-switch-on-connect-symdef.h"
+
+PA_MODULE_AUTHOR("Michael Terry");
+PA_MODULE_DESCRIPTION("When a sink/source is added, switch to it");
+PA_MODULE_VERSION(PACKAGE_VERSION);
+PA_MODULE_LOAD_ONCE(TRUE);
+
+static const char* const valid_modargs[] = {
+    NULL,
+};
+
+struct userdata {
+    pa_hook_slot
+        *sink_put_slot,
+        *source_put_slot;
+};
+
+static pa_hook_result_t sink_put_hook_callback(pa_core *c, pa_sink *sink, void* userdata) {
+    pa_sink_input *i;
+    uint32_t idx;
+    pa_sink *def;
+    const char *s;
+
+    pa_assert(c);
+    pa_assert(sink);
+
+    /* Don't want to run during startup or shutdown */
+    if (c->state != PA_CORE_RUNNING)
+        return PA_HOOK_OK;
+
+    /* Don't switch to any internal devices */
+    if ((s = pa_proplist_gets(sink->proplist, PA_PROP_DEVICE_BUS))) {
+        if (pa_streq(s, "pci"))
+            return PA_HOOK_OK;
+        else if (pa_streq(s, "isa"))
+            return PA_HOOK_OK;
+    }
+
+    def = pa_namereg_get_default_sink(c);
+    if (def == sink)
+        return PA_HOOK_OK;
+
+    /* Actually do the switch to the new sink */
+    pa_namereg_set_default_sink(c, sink);
+
+    /* Now move all old inputs over */
+    if (pa_idxset_size(def->inputs) <= 0) {
+        pa_log_debug("No sink inputs to move away.");
+        return PA_HOOK_OK;
+    }
+
+    PA_IDXSET_FOREACH(i, def->inputs, idx) {
+        if (i->save_sink)
+            continue;
+
+        if (pa_sink_input_move_to(i, sink, FALSE) < 0)
+            pa_log_info("Failed to move sink input %u \"%s\" to %s.", i->index,
+                        pa_strnull(pa_proplist_gets(i->proplist, PA_PROP_APPLICATION_NAME)), sink->name);
+        else
+            pa_log_info("Sucessfully moved sink input %u \"%s\" to %s.", i->index,
+                        pa_strnull(pa_proplist_gets(i->proplist, PA_PROP_APPLICATION_NAME)), sink->name);
+    }
+
+    return PA_HOOK_OK;
+}
+
+static pa_hook_result_t source_put_hook_callback(pa_core *c, pa_source *source, void* userdata) {
+    pa_source_output *o;
+    uint32_t idx;
+    pa_source *def;
+    const char *s;
+
+    pa_assert(c);
+    pa_assert(source);
+
+    /* Don't want to run during startup or shutdown */
+    if (c->state != PA_CORE_RUNNING)
+        return PA_HOOK_OK;
+
+    /* Don't switch to any internal devices */
+    if ((s = pa_proplist_gets(source->proplist, PA_PROP_DEVICE_BUS))) {
+        if (pa_streq(s, "pci"))
+            return PA_HOOK_OK;
+        else if (pa_streq(s, "isa"))
+            return PA_HOOK_OK;
+    }
+
+    def = pa_namereg_get_default_source(c);
+    if (def == source)
+        return PA_HOOK_OK;
+
+    /* Actually do the switch to the new source */
+    pa_namereg_set_default_source(c, source);
+
+    /* Now move all old outputs over */
+    if (pa_idxset_size(def->outputs) <= 0) {
+        pa_log_debug("No source outputs to move away.");
+        return PA_HOOK_OK;
+    }
+
+    PA_IDXSET_FOREACH(o, def->outputs, idx) {
+        if (o->save_source)
+            continue;
+
+        if (pa_source_output_move_to(o, source, FALSE) < 0)
+            pa_log_info("Failed to move source output %u \"%s\" to %s.", o->index,
+                        pa_strnull(pa_proplist_gets(o->proplist, PA_PROP_APPLICATION_NAME)), source->name);
+        else
+            pa_log_info("Sucessfully moved source output %u \"%s\" to %s.", o->index,
+                        pa_strnull(pa_proplist_gets(o->proplist, PA_PROP_APPLICATION_NAME)), source->name);
+    }
+
+    return PA_HOOK_OK;
+}
+
+int pa__init(pa_module*m) {
+    pa_modargs *ma;
+    struct userdata *u;
+
+    pa_assert(m);
+
+    if (!(ma = pa_modargs_new(m->argument, valid_modargs))) {
+        pa_log("Failed to parse module arguments");
+        return -1;
+    }
+
+    m->userdata = u = pa_xnew(struct userdata, 1);
+
+    /* A little bit later than module-rescue-streams... */
+    u->sink_put_slot = pa_hook_connect(&m->core->hooks[PA_CORE_HOOK_SINK_PUT], PA_HOOK_LATE+30, (pa_hook_cb_t) sink_put_hook_callback, u);
+    u->source_put_slot = pa_hook_connect(&m->core->hooks[PA_CORE_HOOK_SOURCE_PUT], PA_HOOK_LATE+20, (pa_hook_cb_t) source_put_hook_callback, u);
+
+    pa_modargs_free(ma);
+    return 0;
+}
+
+void pa__done(pa_module*m) {
+    struct userdata *u;
+
+    pa_assert(m);
+
+    if (!(u = m->userdata))
+        return;
+
+    if (u->sink_put_slot)
+        pa_hook_slot_free(u->sink_put_slot);
+    if (u->source_put_slot)
+        pa_hook_slot_free(u->source_put_slot);
+
+    pa_xfree(u);
+}
