2411d9accd3b7b31e37fcfe083f06b5e35d84679
Daniel Mack <zonque@gmail.com>
2011-04-22 04:28:11 +0200

    thread-posix: Use pthread_(get|set)name_np() if available
    Newer generations of libpthread have functions to set and get the thread
    names. If available, use them.

--- src/pulsecore/thread-posix.c
+++ src/pulsecore/thread-posix.c
@@ -73,6 +73,8 @@ static void* internal_thread_func(void *userdata) {
 
 #ifdef __linux__
     prctl(PR_SET_NAME, t->name);
+#elif defined(HAVE_PTHREAD_SETNAME_NP) && defined(OS_IS_DARWIN)
+    pthread_setname_np(t->name);
 #endif
 
     t->id = pthread_self();
@@ -177,6 +179,8 @@ void pa_thread_set_name(pa_thread *t, const char *name) {
 
 #ifdef __linux__
     prctl(PR_SET_NAME, name);
+#elif defined(HAVE_PTHREAD_SETNAME_NP) && defined(OS_IS_DARWIN)
+    pthread_setname_np(name);
 #endif
 }
 
@@ -193,6 +197,11 @@ const char *pa_thread_get_name(pa_thread *t) {
             pa_xfree(t->name);
             t->name = NULL;
         }
+    }
+#elif defined(HAVE_PTHREAD_GETNAME_NP) && defined(OS_IS_DARWIN)
+    if (!t->name) {
+        t->name = pa_xmalloc0(17);
+        pthread_getname_np(t->id, t->name, 16);
     }
 #endif
 
