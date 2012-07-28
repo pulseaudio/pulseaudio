dbf176acbdc4a6dccdd870348cdedf7a9d8d963d
Deng Zhengrong <dzrongg@gmail.com>
2012-07-28 21:25:58 +0800

    tests: modify channelmap-tets to use 'check' framework

--- src/tests/channelmap-test.c
+++ src/tests/channelmap-test.c
@@ -2,12 +2,15 @@
 #include <config.h>
 #endif
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
 
+#include <check.h>
+
 #include <pulse/channelmap.h>
 
-int main(int argc, char *argv[]) {
+START_TEST (channelmap_test) {
     char cm[PA_CHANNEL_MAP_SNPRINT_MAX];
     pa_channel_map map, map2;
 
@@ -29,9 +32,27 @@ int main(int argc, char *argv[]) {
 
     pa_channel_map_parse(&map2, cm);
 
-    assert(pa_channel_map_equal(&map, &map2));
+    fail_unless(pa_channel_map_equal(&map, &map2));
 
     pa_channel_map_parse(&map2, "left,test");
+}
+END_TEST
 
-    return 0;
+int main(int argc, char *argv[]) {
+    int failed = 0;
+    Suite *s;
+    TCase *tc;
+    SRunner *sr;
+
+    s = suite_create("Channel Map");
+    tc = tcase_create("channelmap");
+    tcase_add_test(tc, channelmap_test);
+    suite_add_tcase(s, tc);
+
+    sr = srunner_create(s);
+    srunner_run_all(sr, CK_NORMAL);
+    failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+
+    return (failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
