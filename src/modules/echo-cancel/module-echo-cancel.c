c43e88553735d480aaf0b1525c64a37beb1d38f4
Peter Meerwald <p.meerwald@bct-electronic.com>
2013-02-13 17:26:52 +0100

    echo-cancel: Output echo canceller name if invalid
    Signed-off-by: Peter Meerwald <p.meerwald@bct-electronic.com>

--- src/modules/echo-cancel/module-echo-cancel.c
+++ src/modules/echo-cancel/module-echo-cancel.c
@@ -1623,7 +1623,7 @@ static int init_common(pa_modargs *ma, struct userdata *u, pa_sample_spec *sourc
 
     ec_string = pa_modargs_get_value(ma, "aec_method", DEFAULT_ECHO_CANCELLER);
     if ((ec_method = get_ec_method_from_string(ec_string)) < 0) {
-        pa_log("Invalid echo canceller implementation");
+        pa_log("Invalid echo canceller implementation '%s'", ec_string);
         goto fail;
     }
 
