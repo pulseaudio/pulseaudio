764eabd1092c825d0633f7eb471ca2822e91994a
Peter Meerwald <p.meerwald@bct-electronic.com>
2013-02-13 17:26:54 +0100

    echo-cancel: Fix zeroing of w in AEC_leaky()
    bug probably caused by alignment requirement; sizeof(a->w) is a pointer, sizeof(a->w_arr) is an array
    
    Signed-off-by: Peter Meerwald <p.meerwald@bct-electronic.com>

--- src/modules/echo-cancel/adrian-aec.c
+++ src/modules/echo-cancel/adrian-aec.c
@@ -174,7 +174,7 @@ static void AEC_leaky(AEC *a)
     } else if (1 == a->hangover) {
       --(a->hangover);
       // My Leaky NLMS is to erase vector w when hangover expires
-      memset(a->w, 0, sizeof(a->w));
+      memset(a->w_arr, 0, sizeof(a->w_arr));
     }
   }
 }
