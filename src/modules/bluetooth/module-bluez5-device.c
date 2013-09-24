da6195760245914c366430e6f6a3d8e4807db790
João Paulo Rechi Vita <jprvita@openbossa.org>
2013-09-24 19:45:41 -0300

    bluetooth: Get BlueZ 5 device object
    Get the remote device information stored in pa_bluetooth_discovery. This
    also creates the mandatory parameter 'path' for module-bluez5-device,
    which is used to inform the object path of the remote device in BlueZ on
    the module load.

--- src/modules/bluetooth/module-bluez5-device.c
+++ src/modules/bluetooth/module-bluez5-device.c
@@ -25,6 +25,7 @@
 #endif
 
 #include <pulsecore/module.h>
+#include <pulsecore/modargs.h>
 
 #include "bluez5-util.h"
 
@@ -34,10 +35,74 @@ PA_MODULE_AUTHOR("João Paulo Rechi Vita");
 PA_MODULE_DESCRIPTION("BlueZ 5 Bluetooth audio sink and source");
 PA_MODULE_VERSION(PACKAGE_VERSION);
 PA_MODULE_LOAD_ONCE(false);
+PA_MODULE_USAGE("path=<device object path>");
+
+static const char* const valid_modargs[] = {
+    "path",
+    NULL
+};
+
+struct userdata {
+    pa_module *module;
+    pa_core *core;
+
+    pa_bluetooth_discovery *discovery;
+    pa_bluetooth_device *device;
+};
 
 int pa__init(pa_module* m) {
+    struct userdata *u;
+    const char *path;
+    pa_modargs *ma;
+
+    pa_assert(m);
+
+    m->userdata = u = pa_xnew0(struct userdata, 1);
+    u->module = m;
+    u->core = m->core;
+
+    if (!(ma = pa_modargs_new(m->argument, valid_modargs))) {
+        pa_log_error("Failed to parse module arguments");
+        goto fail;
+    }
+
+    if (!(path = pa_modargs_get_value(ma, "path", NULL))) {
+        pa_log_error("Failed to get device path from module arguments");
+        goto fail;
+    }
+
+    if (!(u->discovery = pa_bluetooth_discovery_get(m->core)))
+        goto fail;
+
+    if (!(u->device = pa_bluetooth_discovery_get_device_by_path(u->discovery, path))) {
+        pa_log_error("%s is unknown", path);
+        goto fail;
+    }
+
+    pa_modargs_free(ma);
+
     return 0;
+
+fail:
+
+    if (ma)
+        pa_modargs_free(ma);
+
+    pa__done(m);
+
+    return -1;
 }
 
 void pa__done(pa_module *m) {
+    struct userdata *u;
+
+    pa_assert(m);
+
+    if (!(u = m->userdata))
+        return;
+
+    if (u->discovery)
+        pa_bluetooth_discovery_unref(u->discovery);
+
+    pa_xfree(u);
 }
