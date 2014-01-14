298ffffd1a3cf5ac4fbfe9cf793a5eb59162f892
Ben Brewer <ben.brewer@codethink.co.uk>
2014-01-14 12:26:47 +0000

    rtp-send: Remove "don't inhibit auto-suspend" flag
    A source is no longer considered idle in pa_source_check_suspend when
    module-rtp-send is consuming data from it.

--- src/modules/rtp/module-rtp-send.c
+++ src/modules/rtp/module-rtp-send.c
@@ -377,7 +377,6 @@ int pa__init(pa_module*m) {
     pa_source_output_new_data_set_source(&data, s, false);
     pa_source_output_new_data_set_sample_spec(&data, &ss);
     pa_source_output_new_data_set_channel_map(&data, &cm);
-    data.flags = PA_SOURCE_OUTPUT_DONT_INHIBIT_AUTO_SUSPEND;
 
     pa_source_output_new(&o, m->core, &data);
     pa_source_output_new_data_done(&data);
