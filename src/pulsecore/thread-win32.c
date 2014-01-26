a64eead8767932141b2ee54a6c689d7c45409705
Tanu Kaskinen <tanu.kaskinen@linux.intel.com>
2014-01-26 19:11:07 +0200

    thread-win32: Implement pa_thread_free_nojoin()
    This fixes a build error with mingw32:
    
    pulsecore/.libs/libpulsecommon_4.99_la-lock-autospawn.o: In function `unref':
    /home/abuild/rpmbuild/BUILD/pulseaudio-4.99.2/src/pulsecore/lock-autospawn.c:123: undefined reference to `pa_thread_free_nojoin'
    collect2: error: ld returned 1 exit status
    
    pa_thread_free_nojoin() was initially only implemented for the pthread
    based pa_thread backend, because it was incorrectly assumed that
    autospawning (the only user of pa_thread_free_nojoin()) is not used on
    Windows.
    
    Reported-By: Michael DePaulo <mikedep333@gmail.com>

--- src/pulsecore/thread-win32.c
+++ src/pulsecore/thread-win32.c
@@ -109,6 +109,13 @@ void pa_thread_free(pa_thread *t) {
     pa_xfree(t);
 }
 
+void pa_thread_free_nojoin(pa_thread *t) {
+    pa_assert(t);
+
+    CloseHandle(t->thread);
+    pa_xfree(t);
+}
+
 int pa_thread_join(pa_thread *t) {
     assert(t);
 
