5effc834790786c456952beb7aaea7df75053889
Ondrej Holecek <aaannz@gmail.com>
2014-11-26 14:14:51 +0100

    update FSF addresses to FSF web page
    FSF addresses used in PA sources are no longer valid and rpmlint
    generates numerous warnings during packaging because of this.
    This patch changes all FSF addresses to FSF web page according to
    the GPL how-to: https://www.gnu.org/licenses/gpl-howto.en.html
    
    Done automatically by sed-ing through sources.

--- src/pulse/utf8.c
+++ src/pulse/utf8.c
@@ -15,9 +15,7 @@
   Lesser General Public License for more details.
 
   You should have received a copy of the GNU Lesser General Public
-  License along with PulseAudio; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-  USA.
+  License along with PulseAudio; if not, see <http://www.gnu.org/licenses/>.
 ***/
 
 /* This file is based on the GLIB utf8 validation functions. The
@@ -39,9 +37,7 @@
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifdef HAVE_CONFIG_H
