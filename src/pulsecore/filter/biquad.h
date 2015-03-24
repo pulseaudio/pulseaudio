3538e6636edc1ba0b75e7409db618dbb7fe79d3e
David Henningsson <david.henningsson@canonical.com>
2015-03-24 10:29:14 +0100

    lfe-filter: Cleanup and refactor
     - Remove imported dead code
     - Fix compiler warnings
     - Fix non-GCC compiler compilation (use more portable macros)
     - Change lr4 struct to include a biquad struct
    
    Thanks to Alexander Patrakov for suggesting many of these changes.
    
    Signed-off-by: David Henningsson <david.henningsson@canonical.com>

--- src/pulsecore/filter/biquad.h
+++ src/pulsecore/filter/biquad.h
@@ -21,21 +21,12 @@ extern "C" {
 struct biquad {
 	float b0, b1, b2;
 	float a1, a2;
-	float x1, x2;
-	float y1, y2;
 };
 
 /* The type of the biquad filters */
 enum biquad_type {
-	BQ_NONE,
 	BQ_LOWPASS,
 	BQ_HIGHPASS,
-	BQ_BANDPASS,
-	BQ_LOWSHELF,
-	BQ_HIGHSHELF,
-	BQ_PEAKING,
-	BQ_NOTCH,
-	BQ_ALLPASS
 };
 
 /* Initialize a biquad filter parameters from its type and parameters.
@@ -44,11 +35,8 @@ enum biquad_type {
  *    type - The type of the biquad filter.
  *    frequency - The value should be in the range [0, 1]. It is relative to
  *        half of the sampling rate.
- *    Q - Quality factor. See Web Audio API for details.
- *    gain - The value is in dB. See Web Audio API for details.
  */
-void biquad_set(struct biquad *bq, enum biquad_type type, double freq, double Q,
-		double gain);
+void biquad_set(struct biquad *bq, enum biquad_type type, double freq);
 
 #ifdef __cplusplus
 } /* extern "C" */
--- src/pulsecore/filter/crossover.h
+++ src/pulsecore/filter/crossover.h
@@ -6,10 +6,7 @@
 #ifndef CROSSOVER_H_
 #define CROSSOVER_H_
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
+#include "biquad.h"
 /* An LR4 filter is two biquads with the same parameters connected in series:
  *
  * x -- [BIQUAD] -- y -- [BIQUAD] -- z
@@ -18,8 +15,7 @@ extern "C" {
  * The variable [xyz][12] keep the history values.
  */
 struct lr4 {
-	float b0, b1, b2;
-	float a1, a2;
+	struct biquad bq;
 	float x1, x2;
 	float y1, y2;
 	float z1, z2;
@@ -29,48 +25,5 @@ void lr4_set(struct lr4 *lr4, enum biquad_type type, float freq);
 
 void lr4_process_float32(struct lr4 *lr4, int samples, int channels, float *src, float *dest);
 void lr4_process_s16(struct lr4 *lr4, int samples, int channels, short *src, short *dest);
-
-
-/* Three bands crossover filter:
- *
- * INPUT --+-- lp0 --+-- lp1 --+---> LOW (0)
- *         |         |         |
- *         |         \-- hp1 --/
- *         |
- *         \-- hp0 --+-- lp2 ------> MID (1)
- *                   |
- *                   \-- hp2 ------> HIGH (2)
- *
- *            [f0]       [f1]
- *
- * Each lp or hp is an LR4 filter, which consists of two second-order
- * lowpass or highpass butterworth filters.
- */
-struct crossover {
-	struct lr4 lp[3], hp[3];
-};
-
-/* Initializes a crossover filter
- * Args:
- *    xo - The crossover filter we want to initialize.
- *    freq1 - The normalized frequency splits low and mid band.
- *    freq2 - The normalized frequency splits mid and high band.
- */
-void crossover_init(struct crossover *xo, float freq1, float freq2);
-
-/* Splits input samples to three bands.
- * Args:
- *    xo - The crossover filter to use.
- *    count - The number of input samples.
- *    data0 - The input samples, also the place to store low band output.
- *    data1 - The place to store mid band output.
- *    data2 - The place to store high band output.
- */
-void crossover_process(struct crossover *xo, int count, float *data0,
-		       float *data1, float *data2);
-
-#ifdef __cplusplus
-} /* extern "C" */
-#endif
 
 #endif /* CROSSOVER_H_ */
