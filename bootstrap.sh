08959f83d0816fa890452550ea2f0dbd7ebf1205
Ahmed S. Darwish <darwish.07@gmail.com>
2015-09-19 01:59:02 +0200

    build-sys: bootstrap.sh: Do a make only if configure has succeeded
    Otherwise the important configure script error messages get buried
    by the "make clean" output.
    
    Signed-off-by: Ahmed S. Darwish <darwish.07@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -50,6 +50,6 @@ autopoint --force
 AUTOPOINT='intltoolize --automake --copy' autoreconf --force --install --verbose
 
 if test "x$NOCONFIGURE" = "x"; then
-    CFLAGS="$CFLAGS -g -O0" ./configure --sysconfdir=/etc --localstatedir=/var --enable-force-preopen "$@"
-    make clean
+    CFLAGS="$CFLAGS -g -O0" ./configure --sysconfdir=/etc --localstatedir=/var --enable-force-preopen "$@" && \
+        make clean
 fi
