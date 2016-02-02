2086fc82b6fcdb2c85e8867874b1168bac6ecb48
Jungsup Lee <jungsup4.lee@samsung.com>
2016-02-02 13:47:57 +0900

    rescue-streams : Fix a typo
    Fix a typo in log message.
    
    Signed-off-by: Jungsup Lee <jungsup4.lee@samsung.com>

--- src/modules/module-rescue-streams.c
+++ src/modules/module-rescue-streams.c
@@ -309,7 +309,7 @@ static pa_hook_result_t source_output_move_fail_hook_callback(pa_core *c, pa_sou
         return PA_HOOK_OK;
 
     } else {
-        pa_log_info("Successfully moved output input %u \"%s\" to %s.", i->index,
+        pa_log_info("Successfully moved source output %u \"%s\" to %s.", i->index,
                     pa_strnull(pa_proplist_gets(i->proplist, PA_PROP_APPLICATION_NAME)), target->name);
         return PA_HOOK_STOP;
     }
