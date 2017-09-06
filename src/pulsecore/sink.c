bd151646ddeb03c02d7e677a91eb09614acde8e1
Tanu Kaskinen <tanuk@iki.fi>
2017-09-06 16:08:29 +0300

    sink: remove priority bonus for the "internal" form factor
    We're supposed to prioritize USB sound cards over PCI sound cards, but
    the priority bonus for the "internal" form factor prevents this from
    happening. Not all (if any) USB sound cards have the form factor
    property set, whereas at least on my laptop the on-board sound card has
    the form factor set to "internal".

--- src/pulsecore/sink.c
+++ src/pulsecore/sink.c
@@ -3558,8 +3558,6 @@ unsigned pa_device_init_priority(pa_proplist *p) {
             priority += 500;
         else if (pa_streq(s, "portable"))
             priority += 450;
-        else if (pa_streq(s, "internal"))
-            priority += 400;
     }
 
     if ((s = pa_proplist_gets(p, PA_PROP_DEVICE_BUS))) {
