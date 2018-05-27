f17bcb0ad5f97d43530dd2f06922729270e331a1
Lyndon Brown <jnqnfe@gmail.com>
2018-05-27 05:14:04 +0100

    scache: pa_context_play_sample_with_proplist: clarify proplist param
    Existing documentation was unclear about which property list would be the
    one changed (merged into), making it seem (along with the non-const
    proplist pointer param, which needs changing seperately), that the proplist
    object for which a pointer is given will be the one merged into, instead of
    the internal cached entry's proplist.

--- src/pulse/scache.h
+++ src/pulse/scache.h
@@ -116,7 +116,7 @@ pa_operation* pa_context_play_sample_with_proplist(
         const char *name                /**< Name of the sample to play */,
         const char *dev                 /**< Sink to play this sample on */,
         pa_volume_t volume              /**< Volume to play this sample with. Starting with 0.9.15 you may pass here PA_VOLUME_INVALID which will leave the decision about the volume to the server side, which is a good idea.  */ ,
-        pa_proplist *proplist           /**< Property list for this sound. The property list of the cached entry will be merged into this property list */,
+        pa_proplist *proplist           /**< Property list for this sound. The property list of the cached entry will have this merged into it. */,
         pa_context_play_sample_cb_t cb  /**< Call this function after successfully starting playback, or NULL */,
         void *userdata                  /**< Userdata to pass to the callback */);
 
