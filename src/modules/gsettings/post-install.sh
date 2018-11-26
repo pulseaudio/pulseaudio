8146f87808b4b4f9cca1e1558fbfa344fd53d013
Arnaud Rebillout <arnaud.rebillout@collabora.com>
2018-11-26 14:11:47 +0700

    meson: Fix various install scripts
    Signed-off-by: Arnaud Rebillout <arnaud.rebillout@collabora.com>

--- src/modules/gsettings/post-install.sh
+++ src/modules/gsettings/post-install.sh
@@ -5,5 +5,8 @@ set -u
 
 DATADIR="$1"
 
+# Package managers set this so we don't need to run
+if [ "${DESTDIR:-}" ]; then exit 0; fi
+
 echo "Compiling GSettings schemas..."
 glib-compile-schemas "$DATADIR/glib-2.0/schemas"
