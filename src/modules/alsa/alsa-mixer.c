dee7674c7f8906e2266d7fb75615694e436b703e
scootergrisen <scootergrisen@gmail.com>
2019-02-15 15:08:10 +0000

    Remove space in "Digital Passthrough  (IEC958)"

--- src/modules/alsa/alsa-mixer.c
+++ src/modules/alsa/alsa-mixer.c
@@ -4085,7 +4085,7 @@ static int mapping_verify(pa_alsa_mapping *m, const pa_channel_map *bonus) {
         { "analog-surround-70",     N_("Analog Surround 7.0") },
         { "analog-surround-71",     N_("Analog Surround 7.1") },
         { "iec958-stereo",          N_("Digital Stereo (IEC958)") },
-        { "iec958-passthrough",     N_("Digital Passthrough  (IEC958)") },
+        { "iec958-passthrough",     N_("Digital Passthrough (IEC958)") },
         { "iec958-ac3-surround-40", N_("Digital Surround 4.0 (IEC958/AC3)") },
         { "iec958-ac3-surround-51", N_("Digital Surround 5.1 (IEC958/AC3)") },
         { "iec958-dts-surround-51", N_("Digital Surround 5.1 (IEC958/DTS)") },
