86074557a2381643e40d0f18f0108ad2709d5fd0
scootergrisen <scootergrisen@gmail.com>
2019-02-16 01:18:22 +0000

    Replace "!" to "." so string is identical with other string.

--- src/modules/alsa/alsa-source.c
+++ src/modules/alsa/alsa-source.c
@@ -691,7 +691,7 @@ static int unix_read(struct userdata *u, pa_usec_t *sleep_usec, bool polled, boo
             if (polled)
                 PA_ONCE_BEGIN {
                     char *dn = pa_alsa_get_driver_name_by_pcm(u->pcm_handle);
-                    pa_log(_("ALSA woke us up to read new data from the device, but there was actually nothing to read!\n"
+                    pa_log(_("ALSA woke us up to read new data from the device, but there was actually nothing to read.\n"
                              "Most likely this is a bug in the ALSA driver '%s'. Please report this issue to the ALSA developers.\n"
                              "We were woken up with POLLIN set -- however a subsequent snd_pcm_avail() returned 0 or another value < min_avail."),
                            pa_strnull(dn));
